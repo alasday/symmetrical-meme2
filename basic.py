6d0b3883884ffe26b9709b62d578b63e1d2558fe
OOYUKIOO <yuxuan.chen.up@gmail.com>
2016-10-18 20:09:53 -0400

    Modified, add function to generate insert statement

--- basic.py
+++ basic.py
@@ -1,13 +1,25 @@
 import csv
 import sqlite3
 import sys
+import os
 
+#database name
 f="avrgs.db"
 
+#remove database file
+os.remove(f)
+
+#create connection with database
 db = sqlite3.connect(f)
 c = db.cursor()
 
-name = sys.argv[1]
+name=""
+#prompt user to give a student name if the user didn't
+try:
+    name = sys.argv[1]
+except IndexError:
+    print ("\n >>Please enter a student's name.\n")
+
 
 #read peeps.csv & courses.csv into csv reader
 peeps = csv.DictReader(open("peeps.csv"))
@@ -15,18 +27,38 @@ courses = csv.DictReader(open("courses.csv"))
 
 
 #query statement for each csv file
-peepsQ = "CREATE TABLE students (name TEXT, age INTEGER, id INTEGER)"
-coursesQ = "CREATE TABLE courses (code TEXT, mark INTEGER, id INTEGER)"
+peepsQ = """
+         CREATE TABLE students 
+                (name TEXT, age INTEGER, id INTEGER)
+         """
+
+coursesQ = """
+           CREATE TABLE courses 
+                  (code TEXT, mark INTEGER, id INTEGER)
+           """
 
 #create table for each csv file
 c.execute(peepsQ)
 c.execute(coursesQ)
 
+#SELECT statements for each table
+def insertStmt(data, table, fone, ftwo, fthree):
+    stmt = "INSERT INTO " + table + " VALUES ('" + data[fone] + "'," + data[ftwo] + "," + data[fthree] + ")"
+    return stmt
+
+
 #populate each table
 for data in peeps:
-    c.execute("INSERT INTO students VALUES ('"+data["name"]+"',"+data["age"]+","+data["id"]+")")
+    c.execute(insertStmt(data, "students", "name", "age", "id"))
 
 for data in courses:
-    c.execute("INSERT INTO courses VALUES ('"+data["code"]+"',"+data["mark"]+","+data["id"]+")")
+    c.execute(insertStmt(data, "courses", "code", "mark", "id"))
 
-
+'''
+for item in c.execute("""
+               SELECT age,id 
+               FROM students
+               WHERE name='
+               """+name+"'"):
+    print item
+'''
